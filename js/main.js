36e1a354f9fa5722aba2d62df7714baef4aa5c3a
Amanda Gutmann <abgutmann1@gmail.com>
2022-06-22 18:06:45 -0400

    fixed localStorage issue + code organization

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
+// How to Play WAR?
+// Okay, War is a card game where two players take a card out and put it in front of them. The one with the higher value card wins and takes both their own card and their opponents. If both players present a card that has the same value as their opponents, then WAR happens:
+
+// If WAR happens, each player puts three cards face down, and then places a final card face up. The player with the highest value card takes all the cards and puts them on the bottom of their pile. If there is a stalemate again, then WAR repeats until one of the cards is higher than the other, in which case the winner takes them all.
+
+// The winner of the game is decided when all of the cards of one side are owned by either the bot or the player.
+
+// Notes
+  // There should be one deck, which is divided in two between the Player and the Computer.
+  // Player and Computer have one pile of cards each, which is where they draw cards from and where they place their earned cards on the bottom.
+  // In the alt value of the images of Player and Computer, it should display the value of each card as in: `{number} of  {card type}` (for example: "Six of Clubs") 
+
+
+// To-do list:
+// TODO Develop warTime()
+// TODO Work on a simple design for the application
+// TODO Hide the start-button and alt images and set it to only appear after the deck-button has been clicked on
+// TODO Modify the images alt value to reflect the card being shown at the time
+
+
+
+// On page load
 let deckId = "";
 
-fetch("https://deckofcardsapi.com/api/deck/new/")
+// Adds event listener to deck-button, executes splitTheDeck
+document.getElementById("deck-button").addEventListener("click", splitTheDeck)
+
+// Saves the deckId into local storage - user will be able to resume the simulation from where they left it.
+function splitTheDeck() {
+  fetch("https://deckofcardsapi.com/api/deck/new/")
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
         deckId = data.deck_id
+        localStorage.setItem("deckId", deckId)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+    }
 
-
-// Setting the deckId in localStorage - Figure out how to set it properly so that the deckId is the same for the user.
-// window.localStorage.setItem("deckId", `${deckId}`)
-
-
-document.querySelector('button').addEventListener('click', drawTwoCards)
+// Adds event listener to start-button, executes drawTwoCards
+document.getElementById("start-button").addEventListener('click', drawTwoCards)
 
 function drawTwoCards(){
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+  const url = `https://deckofcardsapi.com/api/deck/${localStorage.deckId}/draw/?count=2`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
@@ -71,12 +96,6 @@ function drawTwoCards(){
       });
 }
 
-// Pseudocode dis bitch
-// Okay, War is a card game where two players take a card out and put it in front of them. The one with the higher value card wins and takes both their own card and their opponents. If both players present a card that has the same value as their opponents, then WAR happens:
-
-
-// If WAR happens, each player puts three cards face down, and then places a final card face up. The player with the highest value card takes all the cards and puts them on the bottom of their pile. If there is a stalemate again, then WAR repeats until one of the cards is higher than the other, in which case the winner takes them all.
-
 function warTime() {
   // player & bot draw three cards each
   if (playerCard > botCard) {
@@ -91,10 +110,3 @@ function warTime() {
     console.log("An error has ocurred.")
   } 
 }
-
-// The winner of the game is decided when all of the cards of one side are owned by either the bot or the player.
-
-// Notes
-// There should be one deck, which is divided in two between the Player and the Computer.
-// Player and Computer have one pile of cards each, which is where they draw cards from and where they place their earned cards on the bottom.
-// In the alt value of the images of Player and Computer, it should display the value of each card as in: `{number} of  {card type}` (for example: "Six of Clubs") 
